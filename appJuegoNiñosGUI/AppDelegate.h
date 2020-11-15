b668f04126c5425fe1864585f2bbf18df551fc33
betoFlores58 <albertfr@live.com.mx>
2020-11-14 22:41:11 -0600

    Initial Commit

---
+++ appJuegoNiñosGUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  appJuegoNiñosGUI
+//
+//  Created by Alberto Flores on 14/11/20.
+//  Copyright © 2020 Alberto Flores. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ appJuegoNiñosGUI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  appJuegoNiñosGUI
+//
+//  Created by Alberto Flores on 14/11/20.
+//  Copyright © 2020 Alberto Flores. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
